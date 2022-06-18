dc1dd74036c6f9a351669c409fcb54ab00688742
mateo <mateo.menghini@gmail.com>
2022-06-18 20:58:55 -0300

    tabs

--- app/src/main/java/com/example/smarthausapp/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/smarthausapp/ui/main/SectionsPagerAdapter.kt
@@ -7,7 +7,6 @@ import androidx.fragment.app.FragmentPagerAdapter
 import com.example.smarthausapp.R
 
 private val TAB_TITLES = arrayOf(
-    R.string.tab_text_1,
     R.string.tab_text_2,
     R.string.tab_text_3
 )
@@ -30,6 +29,6 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
     }
 
     override fun getCount(): Int {
-        return 3
+        return 2
     }
 }
